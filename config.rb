5bda672d182e25c86c979f7da50b793c96c834ba
evild70 <dlahay@gmail.com>
2014-03-04 22:56:14 -0600

    Changes

--- config.rb
+++ config.rb
@@ -6,7 +6,7 @@ require 'sass-globbing'
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
-sass_dir = "css/scss"
+sass_dir = "css/sass"
 images_dir = "images"
 javascripts_dir = "js"
 
